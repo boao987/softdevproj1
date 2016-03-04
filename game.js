aa61f81ff47550b83efecbffbdd5aebbd863eed5
boao987 <schooloffranklin12@gmail.com>
2016-03-04 18:18:14 +0000

    funtion layout

--- game.js
+++ game.js
@@ -12,7 +12,7 @@ var ballMove = function(){
     
 }
 
-var shooterDirection = function(){
+var shooterDirection = function(){//determines where the arrow points
     
 }
 
@@ -20,15 +20,15 @@ var isPopping = function(){//will be used in collision
     
 }
 
-var isDead = function(){
+var isDead = function(){//checks if there are balls near the bottom
     
 }
 
-var moveDown = function(){
+var moveDown = function(){//creates a newRow after a few turns
     
 }
 
-var newRow = function(){//will be used in newGame
+var newRow = function(){//will be used in newGame and moveDown
     
 }
 
@@ -36,11 +36,11 @@ var newBall = function(){//will be used in newRow
     
 }
 
-var newGame = function(){//
+var newGame = function(){//will be used in runGame
     points = 0;
     
 }
 
-var runGame = function(){
+var runGame = function(){//will always be running
     
 }

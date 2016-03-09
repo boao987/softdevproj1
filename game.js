fd0c293af6c103482ba2b2d2f323f2dcd3daf9f4
Sean Chu <StridentStorm@users.noreply.github.com>
2016-03-09 12:08:09 -0500

    Fixed

--- game.js
+++ game.js
@@ -22,9 +22,13 @@ var ballMove = function(e){
     
         return findgcd(b, a % b);
         };
+        var xcor = parseInt(currBall.getAttribute("cx"));
+        var ycor = parseInt(currBall.getAttribute("cy"));
         var gcd = findgcd(Math.abs(e.offsetX - parseInt (currBall.getAttribute("cx"))), Math.abs(e.offsetY - parseInt(currBall.getAttribute("cy"))));
         var xchange = (e.offsetX - parseInt(currBall.getAttribute("cx")) / gcd);
         var ychange = (e.offsetY - parseInt(currBall.getAttribute("cy")) / gcd);
+        console.log(xchange);
+        console.log(ychange);
         var animate = function() {
             //Check if the ball collided with something
             if (collision){
@@ -39,14 +43,17 @@ var ballMove = function(e){
                 ychange = ychange * -1;
             }
             //If nothing happens, move the ball
-            xcor = xcor + xchange/1000;
-            ycor = ycor + ychange/1000;
+            xcor = xcor + xchange/100;
+            ycor = ycor + ychange/100;
+            console.log(xcor);
+            console.log(ycor);
             currBall.setAttribute("cx", xcor);
             currBall.setAttribute("cy", ycor);
             //Add the ball to the element
             svg.appendChild(currBall);
         };
         frameid = setInterval(animate, 10);
+        //animate();
     };
 
 var shooterDirection = function(){//determines where the arrow points
@@ -125,12 +132,12 @@ var runGame = function(){//will always be running
     svg.appendChild((startButton));
 };
 
-runGame();
+//runGame();
 //Bug Testing Stuff
-//currBall = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-//currBall.setAttribute("cx", 250);
-//currBall.setAttribute("cy", 250);
-//currBall.setAttribute("fill", "#ff0000");
-//currBall.setAttribute("r", 20);
-//svg.appendChild(currBall);
+currBall = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+currBall.setAttribute("cx", 250);
+currBall.setAttribute("cy", 250);
+currBall.setAttribute("fill", "#ff0000");
+currBall.setAttribute("r", 20);
+svg.appendChild(currBall);
 svg.addEventListener("click", ballMove);
